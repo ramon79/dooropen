43ec66cadf644d41753ff1552105391b287bbbc2
ramon79 <chanydang@gmail.com>
2019-02-10 21:14:48 +0900

    first

---
+++ Assets/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+	public GameObject Door;
+
+	public bool doorIsOpening;
+
+	void OnMouseDown()
+	{
+		doorIsOpening = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(doorIsOpening == true)
+		{
+			Door.transform.Translate (Vector3.up * Time.deltaTime);
+
+		}
+		if(Door.transform.position.y > 7f)
+		{
+			doorIsOpening = false;
+	    }
+	}
+		
+
+}
